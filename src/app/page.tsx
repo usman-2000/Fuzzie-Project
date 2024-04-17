68c47e5b7fa4d3622dad4375ff5c226926d64fba
usman-2000 <usmanrahim2000@gmail.com>
2024-04-17 16:10:08 +0500

    Cards system in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import { HeroParallax } from "@/components/global/connect-parallax";
 import { ContainerScroll } from "@/components/global/container-scroll-animation";
+import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
 import Navbar from "@/components/global/navbar";
 import { Button } from "@/components/ui/button";
+import { clients, products } from "@/lib/constant";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <Navbar />
       <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
         <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
@@ -26,8 +30,17 @@ export default function Home() {
                 </h1>
               </div>
             }
-          ></ContainerScroll>
+          />
         </div>
+      </section>
+      <InfiniteMovingCards
+        className="md:mt-[18rem] mt-[-100px]"
+        items={clients}
+        direction="right"
+        speed="slow"
+      />
+      <section>
+        <HeroParallax products={products}></HeroParallax>
       </section>
     </main>
   );
