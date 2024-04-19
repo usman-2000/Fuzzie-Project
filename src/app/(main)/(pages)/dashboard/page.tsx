8917c301fbafc6028070e87ea8baa4cdbceec102
usman-2000 <usmanrahim2000@gmail.com>
2024-04-19 19:23:16 +0500

    Dashboard

--- src/app/(main)/(pages)/dashboard/page.tsx
+++ src/app/(main)/(pages)/dashboard/page.tsx
@@ -3,7 +3,7 @@ const DashboardPage = () => {
   return (
     <div className="flex flex-col gap-4 relative">
       <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b">
-        Dashboard
+        Dashboard 1
       </h1>
     </div>
   );
