84183a3519cf1ebf98b1e8dbbd17c81f19317477
usman-2000 <usmanrahim2000@gmail.com>
2024-04-22 01:30:26 +0500

    Dashboards tooltip of shadcn

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -9,11 +9,12 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { menuOptions } from "@/lib/constant";
+import clsx from "clsx";
 
 type Props = {};
 
 const MenuOptions = (props: Props) => {
-  const pathname = usePathname();
+  const pathName = usePathname();
   return (
     <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
       <div className="flex items-center justify-center flex-col gap-8">
