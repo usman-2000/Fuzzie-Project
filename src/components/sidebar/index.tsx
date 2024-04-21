33d777525af5fab5bcef6b95aa3dcd95dcfe5311
usman-2000 <usmanrahim2000@gmail.com>
2024-04-21 17:26:10 +0500

    side bar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -6,7 +6,15 @@ type Props = {};
 
 const MenuOptions = (props: Props) => {
   const pathname = usePathname();
-  return <div>MenuOptions</div>;
+  return (
+    <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
+      <div className="flex items-center justify-center flex-col gap-8">
+        <Link className="flex font-bold flex-row " href="/">
+          fuzzie.
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default MenuOptions;
